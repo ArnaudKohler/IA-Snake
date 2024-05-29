cddd5dec3401f792cf948e4d2f4acac4e1be87ad
Lucas BRANCOLINI <lucas.brancolini@efrei.net>
2024-05-29 12:57:24 +0200

    bug correction : food no longer spawn on the snake's tail

--- food.py
+++ food.py
@@ -3,16 +3,18 @@ import random
 
 # Classe Food
 class Food:
-    def __init__(self, GRID_SIZE, CELL_SIZE):
+    def __init__(self, GRID_SIZE, CELL_SIZE, snake, grid_offsets):
         self.position = (0, 0)
         self.grid_size = GRID_SIZE
         self.cell_size = CELL_SIZE
-        self.new_food()
+        self.offset_x, self.offset_y = grid_offsets
+        self.new_food(snake)
         
 
-    def new_food(self):
-        self.position = (random.randint(0, self.grid_size-1), random.randint(0, self.grid_size-1))
+    def new_food(self, snake):
+        while(self.position in snake.positions): #Check that food doesn't spawn on a snake tile
+            self.position = (random.randint(0, self.grid_size-1), random.randint(0, self.grid_size-1))
 
     def draw(self, surface):
-        rect = pygame.Rect(self.position[0] * self.cell_size, self.position[1] * self.cell_size, self.cell_size, self.cell_size)
+        rect = pygame.Rect(self.offset_x + self.position[0] * self.cell_size, self.offset_y + self.position[1] * self.cell_size, self.cell_size, self.cell_size)
         pygame.draw.rect(surface, (255, 0, 0), rect)
